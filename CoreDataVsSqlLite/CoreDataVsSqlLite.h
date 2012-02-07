5bbb139fd66f54ab052fdc87eccddcfedbd0822c
Scott Taylor <scott@railsnewbie.com>
2012-02-07 03:04:16 -0500

    Initial Commit

---
+++ CoreDataVsSqlLite/CoreDataVsSqlLite.h
@@ -0,0 +1,13 @@
+//
+//  CoreDataVsSqlLite.h
+//  CoreDataVsSqlLite
+//
+//  Created by Scott Taylor on 2/7/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CoreDataVsSqlLite : NSObject
+
+@end
---
+++ CoreDataVsSqlLite/CoreDataVsSqlLite.m
@@ -0,0 +1,23 @@
+//
+//  CoreDataVsSqlLite.m
+//  CoreDataVsSqlLite
+//
+//  Created by Scott Taylor on 2/7/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "CoreDataVsSqlLite.h"
+
+@implementation CoreDataVsSqlLite
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
